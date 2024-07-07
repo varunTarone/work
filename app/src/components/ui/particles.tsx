00d7093ee39032da8b2fb96235067795bf4587ed
varunTarone <vr17t@gmail.com>
2024-07-07 22:40:57 +0530

    fix

--- app/src/components/ui/particles.tsx
+++ app/src/components/ui/particles.tsx
@@ -13,6 +13,21 @@ interface ParticlesProps {
   vy?: number;
   containerRef?: React.MutableRefObject<HTMLDivElement | null>;
 }
+function hexToRgb(hex: string): number[] {
+  // Remove the "#" character from the beginning of the hex color code
+  hex = hex.replace("#", "");
+
+  // Convert the hex color code to an integer
+  const hexInt = parseInt(hex, 16);
+
+  // Extract the red, green, and blue components from the hex color code
+  const red = (hexInt >> 16) & 255;
+  const green = (hexInt >> 8) & 255;
+  const blue = hexInt & 255;
+
+  // Return an array of the RGB values
+  return [red, green, blue];
+}
 
 export const Particles: React.FC<ParticlesProps> = ({
   className = "",
@@ -25,8 +40,8 @@ export const Particles: React.FC<ParticlesProps> = ({
   vy = 0,
   containerRef,
 }) => {
-  const canvasContainerRef = containerRef || useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasContainerRef = containerRef || useRef<HTMLDivElement>(null);
   const context = useRef<CanvasRenderingContext2D | null>(null);
   const circles = useRef<any[]>([]);
   const mousePosition = MousePosition();
@@ -241,20 +256,3 @@ export const Particles: React.FC<ParticlesProps> = ({
     </div>
   );
 };
-
-function hexToRgb(hex: string): number[] {
-  // Remove the "#" character from the beginning of the hex color code
-  hex = hex.replace("#", "");
-
-  // Convert the hex color code to an integer
-  const hexInt = parseInt(hex, 16);
-
-  // Extract the red, green, and blue components from the hex color code
-  const red = (hexInt >> 16) & 255;
-  const green = (hexInt >> 8) & 255;
-  const blue = hexInt & 255;
-
-  // Return an array of the RGB values
-  return [red, green, blue];
-}
-

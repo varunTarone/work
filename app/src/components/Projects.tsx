70fed9a1b9ebe80eb9269e19644b0ec6e3aece82
varunTarone <vr17t@gmail.com>
2024-07-07 22:16:45 +0530

    fix: useRef errors

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
     <div className='z-10'>
       <Title title='Projects' description='Check out my latest work' />
       <h2 className="font-semibold text-xl text-neutral-800 dark:text-neutral-500 text-center">
-      I've worked on a variety of projects, here are a few of my favorites.
+      {`I've worked on a variety of projects, here are a few of my favorites.`}
       </h2>
       <div className='md:grid-cols-2 grid grid-cols-1 gap-10 '>
       {project.map((element, index) => {

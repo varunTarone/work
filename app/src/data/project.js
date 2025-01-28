c74eed6b2375f5ff1c573e164ba7c5637f3e2ac0
Varun <var17t@gmail.com>
2025-01-28 21:08:45 +0530

    feat: added more projects

--- app/src/data/project.js
+++ app/src/data/project.js
@@ -1,10 +1,24 @@
 export const project = [
     {
+        heading: 'BrainWave',
+        description: 'A landing page designed for a multimedia AI chatbot SaaS using Vite, React framework and Tailwind CSS.',
+        live:'https://brainwave-landing-rust.vercel.app/',
+        code:'https://github.com/varunTarone/brainwave-landing',
+        image: '/brain.png'
+    },
+    {
         heading: 'Sumz',
         description: 'An AI-powered summarizer that can summarize any article into a few sentences.',
         live:'https://summarizeart.netlify.app/',
         code:'https://github.com/varunTarone/Sumz',
         image: '/sumz.png'
+    },
+    {
+        heading: 'RTC Chat',
+        description: 'A real time chat application built using websocket framework which allows users to create custom rooms to chat with multiple users in real time.',
+        live:'https://rtc-chat-client.vercel.app/',
+        code:'https://github.com/varunTarone/rtc-chat/',
+        image: '/rtc.png'
     },
     {
         heading: 'MovieApp',

4dfbead720ef68bb187c1e9a888263d215b88cf9
varunTarone <var17t@gmail.com>
2025-07-24 23:36:18 +0530

    feat: added project

--- app/src/data/project.js
+++ app/src/data/project.js
@@ -1,4 +1,18 @@
 export const project = [
+    { 
+        heading: 'Klasmic', 
+        description: 'A Real-time AI Teaching and Learning LMS application that allows to Build and Customize AI Companion for learning, teaching, and tutoring.', 
+        live:'https://klasmic.vercel.app/', 
+        code:'https://github.com/varunTarone/klasmic', 
+        image: '/klasmic.png' 
+    },
+    {
+        heading: 'RTC Chat',
+        description: 'A real time chat application built using websocket framework which allows users to create custom rooms to chat with multiple users in real time.',
+        live:'https://rtc-chat-client.vercel.app/',
+        code:'https://github.com/varunTarone/rtc-chat/',
+        image: '/rtc.png'
+    },
     {
         heading: 'Sumz',
         description: 'An AI-powered summarizer that can summarize any article into a few sentences.',
@@ -20,18 +34,4 @@ export const project = [
         code:'https://github.com/varunTarone/saas-landing',
         image: '/saas.png'
     },
-    {
-        heading: 'RTC Chat',
-        description: 'A real time chat application built using websocket framework which allows users to create custom rooms to chat with multiple users in real time.',
-        live:'https://rtc-chat-client.vercel.app/',
-        code:'https://github.com/varunTarone/rtc-chat/',
-        image: '/rtc.png'
-    },
-    {
-        heading: 'MovieApp',
-        description: 'A React-based movie landing page showcasing films from the OMDb API, designed to deliver comprehensive movie list.',
-        live:'https://moviefandom.netlify.app/',
-        code:'https://github.com/varunTarone/movie-app/',
-        image: '/fandom.png'
-    }
 ]
